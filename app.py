cb5ff069371f38786c400d36ad225cecccc58ff4
root <root@ip-172-31-86-93.ec2.internal>
2022-09-24 17:35:11 +0000

    my first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+this is myy new file, creatingon 2nd instance of aws
+commands practise session 1:
+    ssh-keygen <<it will create public/private key in home or root folder>>
+    cat [path as show after ssh-keygen executed]
+    git clone git@github.com:vaibhavbansal1980/test.git <<this is a git command used to clone public repo with file nd folder>>
+    git status <<this command show the git file and folders >>
