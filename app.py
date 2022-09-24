c9e8b64198805db90cc2bfa09cb7d8a731a7d723
vaibhavbansal1980 <vaibhav.mtech@gmail.com>
2022-09-24 17:46:16 +0000

    2 commit

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ commands practise session 1:
     cat [path as show after ssh-keygen executed]
     git clone git@github.com:vaibhavbansal1980/test.git <<this is a git command used to clone public repo with file nd folder>>
     git status <<this command show the git file and folders >>
+
+    My first commit done
+
+    session 2 following commands
+
+    git add app.py    <<created file with vi editor and added to git>>
+    git commit -m "my first commit" <<git commit done,by default at master>>
+    git log         << to check git commit status>>
+    git config --global user.name "vaibhavbansal1980"
+    git config --global user.email "vaibhav.mtech@gmail.com"
+
